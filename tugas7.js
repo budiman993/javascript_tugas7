ab08256015fa195237836d756264304bcb392b4f
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-20 15:47:26 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,11 @@
+var tinggi =[150, 155, 156, 165, 170, 166, 172, 175, 173, 169]
+
+for(var i=0; i<tinggi.length; i++){
+	console.log(tinggi[i])
+}
+
+for(let x of tinggi){
+	console.log(x)
+}
+
+// Hasil dari perulangan biasa dan perulangan foroff sama saja, yang membedakan hanya cara penulisan codenya. perulangan foroff lebih simple digunakan untuk menampilkan array
